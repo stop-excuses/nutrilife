af3c5f01ba55275737e50b45713f304e03908627
Ivan.Milev <ivvmilev@gmail.com>
2026-05-21 08:52:03 +0000

    chore: update offers [kaufland,billa,fantastico,dar] 2026-05-21 08:52 UTC

--- data/all_products.js
+++ data/all_products.js
@@ -1,5 +1,5 @@
 const ALL_PRODUCTS_DATA = {
-  "generated_at": "2026-05-21T07:40:46.542347Z",
+  "generated_at": "2026-05-21T08:51:59.703473Z",
   "total_products": 16710,
   "products": [
     {
@@ -675835,7 +675835,7 @@ const ALL_PRODUCTS_DATA = {
       "is_high_protein": false,
       "is_good_carb": false,
       "is_good_fat": false,
-      "image": "https://media.kaufland.com/images/PPIM/KMO/BG300_20344167_P.jpg",
+      "image": "https://kaufland.media.schwarz/is/image/schwarz/4063367446894_BG_P?JGstbGVnYWN5LW9uc2l0ZS0zJA==",
       "product_id": "kaufland-кош-за-пране-36-л-36000g",
       "last_seen": "2026-05-21",
       "first_seen": "2026-05-18",
