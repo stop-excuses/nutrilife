71e7c387e5d90d4b4b167ffd9fd71addf552adf1
Ivan.Milev <>
2026-05-22 09:36:53 +0300

    Stabilize top navbar on mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nutrilife-v25';
+const CACHE_NAME = 'nutrilife-v27';
 const OFFLINE_URL = 'offline.html';
 const APP_SHELL = [
   './',
@@ -14,8 +14,8 @@ const APP_SHELL = [
   OFFLINE_URL,
   'manifest.json',
   'favicon.svg',
-  'css/style.css?v=33',
-  'js/main.js?v=16',
+  'css/style.css?v=34',
+  'js/main.js?v=17',
   'images/pwa/icon-192.png',
   'images/pwa/icon-512.png',
   'images/pwa/icon-maskable-192.png',
